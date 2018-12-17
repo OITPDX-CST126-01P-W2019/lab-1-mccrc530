5edb6582872df9fdb3f46a7cb7d0b5773a3cafc8
Paula Hannan <paula.hannan@oit.edu>
2018-12-17 11:53:26 -0800

    Update with readme and docx

--- W19_CST126_1/W19_CST126_1.cpp
+++ W19_CST126_1/W19_CST126_1.cpp
@@ -1,21 +1,34 @@
-// W19_CST126_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// W19_CST126_1.cpp : Cyclomatic Complexity
 //
 
 #include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Here are the steps I took when I did the lab.  They are just suggestions.  Please do work incrementally.
+//  Made sure I could open the file.
+//		Checked for a successful open
+//      Had default cout of cout << "Do the work here" << endl;
+//      and then closed.
+//  Added declaration for a function to process the file
+//  added function body which had cout << "Do the work here" << endl;
+//  Made constants for my search keywords 
+//  added read loop which just did a cout of what it read.  (which I got rid of eventually)
+//  counted my variables of the four types within my function.
+//	   Did a simple printout in the function.
+//  Took a break.  I have a program that is at a good stopping point because I compiled and saved at each step.
+//  added a count for all the tokens read.  
+//    Hint: if you slightly more than 83 words on the test file, check to make sure you didn't add any extra spaces or 
+//       extra new lines.   If you have a lot more then 83 words, check to be sure that your count is at the right loop level.
+//  Added a struct with just the filename.   This included adding a new .h and a new .cpp 
+//   refactored the program so that my open function passed in a string with the filename
+//  Changed the Process File Routine to take my new structure.  Printed out the file name only.
+//    moved my total count variable into the structure.  
+//    move my keyword count variable (I'm using an array) to the structure.
+//       this included creating an initialization method within my structure.
+//    moved the printing out of my processing routine and into a print method within the structure.
---
+++ readme.md
@@ -0,0 +1,23 @@
+## CST 126 - CST126_1 Winter 19 - Cyclomatic Complexity
+
+**Class ID:** &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [replace with your id]
+
+**Unresolved Issues:**
++ [Replace this with any known issues.  If it works groovy say that]
+
+**Notes:**
+
+
+
+## Cyclomatic Complexity
+
+### Learning Objectives
+
++  Review file IO
++  String class
++  Includes
++  Structures
+
+#### Background / Inputs / Logic / Outputs / Test Data
+
+See lab1W19.docx
